01caed34cf737c88d82c9dbc8d18fb10beaf055c
Ivan Chan <s3534199@student.rmit.edu.au>
2018-05-22 02:25:15 +1000

    first commit

---
+++ WebApiCore/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiCore.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiCore.Controllers
+{
+    //set route of the api
+    [Route("api/orders")]
+    public class OrdersController : Controller
+    {
+        private readonly OrderContext _context;
+
+        public OrdersController(OrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/orders
+        [HttpGet]
+        public async Task<IEnumerable<Order>> Get()
+        {
+            var orders = await _context.Order.ToArrayAsync();
+
+            return orders;
+        }
+
+        // GET: api/orders/5
+        [HttpGet("{id}")]
+        public async Task<Order> Get(int id)
+        {
+            return await _context.Order.FirstOrDefaultAsync(x => x.ProductID == id);
+        }
+    }
+}
---
+++ WebApiCore/Models/OrderContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace WebApiCore.Models
+{
+    public class OrderContext :DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        { }
+
+        public DbSet<Order> Order { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().ToTable("Order");
+        }
+    }
+}
